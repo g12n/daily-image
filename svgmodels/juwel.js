a8dfc9a61f01389ad4f806eebbf54da684511d07
Michael Gehrmann <57556+g12n@users.noreply.github.com>
2021-08-14 17:02:03 +0200

    fixed typo in juwel

--- svgmodels/juwel.js
+++ svgmodels/juwel.js
@@ -26,6 +26,6 @@ export const juwel = (viewBox, palette= ["#003049","#d62828","#f77f00","#fcbf49"
         svg += `<path d="${path}" fill="${color}"/>`
     })
     svg +="</svg>"
-    return svg;s
+    return svg;
 }
 
